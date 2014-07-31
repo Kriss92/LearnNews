1ab9fc8bddb7490391d1be35ef41462554303765
viktorovam <Antoaneta>
2014-07-31 23:19:15 +0100

    Initial Managers

---
+++ app/src/main/java/com/appchee/learnews/actions/QuestionsManager.java
@@ -0,0 +1,24 @@
+package com.appchee.learnews.actions;
+
+import com.appchee.learnews.beans.QuestionBean;
+
+/**
+ * Created by demouser on 7/31/14.
+ */
+public class QuestionsManager {
+
+
+    public QuestionBean getNextQuestion() {
+        QuestionBean result = null;
+
+
+        return result;
+    }
+
+    public Double answerQuestion(QuestionBean question, boolean correct) {
+        //Update corrects/incorrects count
+
+        //return percentage correct
+        return 0.0;
+    }
+}
