7678b838f7ce1fba6c2afb83718ea985593da7e2
Kriss92 <Cristina>
2014-08-01 02:53:51 +0100

    Created gradient in xml

--- app/src/main/java/com/appchee/learnews/MenuActivity.java
+++ app/src/main/java/com/appchee/learnews/MenuActivity.java
@@ -2,14 +2,19 @@ package com.appchee.learnews;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.appchee.learnews.R;
+
+import java.util.zip.Inflater;
 
 public class MenuActivity extends Activity {
 
@@ -38,7 +43,6 @@ public class MenuActivity extends Activity {
                 //Sign user out
                 break;
             case R.id.action_change_background:
-                Log.d("Something", "Something something background change");
                 startBackgroundDialog();
                 break;
             case R.id.action_about:
@@ -52,25 +56,27 @@ public class MenuActivity extends Activity {
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
         alertDialog.setTitle(R.string.change_background_dialog_question);
         alertDialog.setIcon(R.drawable.ic_launcher);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.dialog_change_background_color, null);
+        alertDialog.setView(v);
         alertDialog.show();
     }
 
     public void newGameMenuButtonClicked(View view) {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
-
     }
 
     public void addQuestionsMenuButtonClicked(View view) {
         Intent intent = new Intent(this, AddQuestionsActivity.class);
         startActivity(intent);
-
     }
 
     public void SavedStoriesMenuButtonClicked(View view) {
         Intent intent = new Intent(this, SavedStoriesActivity.class);
         startActivity(intent);
-
     }
 
 }
